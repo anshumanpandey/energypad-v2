0d3589d75fc70e05db9bc9bc4b99cef95cf593f8
APSonin <anshumanpandey71@gmail.com>
2021-11-19 14:49:58 -0400

    FIX on script

--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -3,7 +3,7 @@
 # Install node.js
 sudo apt-get update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-source ~/.bashrc
 nvm install v14.17.5
+source ~/.bashrc
 
 npm install --global yarn
