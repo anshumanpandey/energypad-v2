fc03550a4ae47071181745397ba9c6a177fc5d75
APSonin <anshumanpandey71@gmail.com>
2021-11-19 15:51:05 -0400

    FIX on script

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -7,7 +7,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install v14.17.5
-npm install --global yarn
+sudo npm install --global yarn
 cd /home/ubuntu/nodejs
 
 sudo yarn install
