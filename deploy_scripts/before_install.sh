14b524b9c6a0d66759c03b282958587a2c518c65
APSonin <anshumanpandey71@gmail.com>
2021-11-19 16:01:44 -0400

    FIX on script

--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -2,11 +2,11 @@
 
 # Install node.js
 sudo apt-get update
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-source ~/.bashrc
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+sudo source ~/.bashrc
+sudo export NVM_DIR="$HOME/.nvm"
+sudo [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+sudo [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 sudo nvm install v14.17.5
 
 sudo npm install -g yarn
