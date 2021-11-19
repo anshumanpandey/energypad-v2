9a856021265f084de5832a927d8c67d4fe2aed35
APSonin <anshumanpandey71@gmail.com>
2021-11-19 15:58:25 -0400

    FIX on script

--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -7,4 +7,6 @@ source ~/.bashrc
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install v14.17.5
+sudo nvm install v14.17.5
+
+sudo npm install -g yarn
