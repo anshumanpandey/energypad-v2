9991ee89b2139fd166bb5906ed31812643159872
APSonin <anshumanpandey71@gmail.com>
2024-10-11 07:08:03 -0500

    ADDED column

---
+++ migrations/20241011120551_tip_user_siteid.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('EnergySavingTipsToBusiness', function (table) {
+    table.integer('siteId', 255).notNullable();
+    table.foreign('siteId').references('Sites.id').onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('EnergySavingTipsToUser', function (table) {
+    table.dropColumn('siteId');
+  });
+}
