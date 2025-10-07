fb863c6ed92cfd2a3dab76dd388556230fdd7c86
leo_9_t <leo_9_t@hotmail.com>
2025-10-07 02:53:01 -0500

    FIX on duplicated values

--- seeds/test/20211212191934_energy_usedin.ts
+++ seeds/test/20211212191934_energy_usedin.ts
@@ -4,20 +4,27 @@ import { buildFakeBusiness, buildFakeSite } from '../../test/testhelp';
 export async function seed(knex: Knex): Promise<void> {
   // Inserts seed entries
   const fuel1 = { id: 1, source: 'Grid Electricity', colorCode: '#4989C6' };
-  await knex('FuelSources').insert([
-    fuel1,
-    { id: 2, source: 'Gas', colorCode: '#7F9EAE' },
-    { id: 3, source: 'Solar PV', colorCode: '#F5B363' },
-    { id: 4, source: 'Petrol', colorCode: '#4D505C' },
-    { id: 5, source: 'Diesel', colorCode: '#E97871' },
-    { id: 6, source: 'Bio Diesel', colorCode: '#68D8DC' },
-  ]);
-  await knex('FuelUses').insert([
-    { id: 105, use: 'Cooling' },
-    { id: 2, use: 'Heating' },
-    { id: 3, use: 'Lighting' },
-    { id: 4, use: 'Powering' },
-  ]);
+  await knex('FuelSources')
+    .insert([
+      fuel1,
+      { id: 2, source: 'Gas', colorCode: '#7F9EAE' },
+      { id: 3, source: 'Solar PV', colorCode: '#F5B363' },
+      { id: 4, source: 'Petrol', colorCode: '#4D505C' },
+      { id: 5, source: 'Diesel', colorCode: '#E97871' },
+      { id: 6, source: 'Bio Diesel', colorCode: '#68D8DC' },
+    ])
+    .onConflict('id')
+    .ignore();
+
+  await knex('FuelUses')
+    .insert([
+      { id: 105, use: 'Cooling' },
+      { id: 2, use: 'Heating' },
+      { id: 3, use: 'Lighting' },
+      { id: 4, use: 'Powering' },
+    ])
+    .onConflict('id')
+    .ignore();
 
   await knex('Businesses').insert([buildFakeBusiness({ id: 148, email: 'mail482@mail.com' })]);
 
