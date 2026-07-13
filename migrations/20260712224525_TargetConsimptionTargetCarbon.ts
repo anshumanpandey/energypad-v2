d22ea8d6e1b39481a68881644125bfa37ecf1922
leo_9_t <leo_9_t@hotmail.com>
2026-07-12 19:09:41 -0500

    REMOVE non existing table from unique index

--- migrations/20260712224525_TargetConsimptionTargetCarbon.ts
+++ migrations/20260712224525_TargetConsimptionTargetCarbon.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('TargetConsumptionFuelConversion', function (table) {
     table.float('targetCarbon');
 
-    table.unique(['date', 'fuelSourceId', 'siteId']);
+    table.unique(['fuelSourceId', 'siteId']);
   });
 }
 
